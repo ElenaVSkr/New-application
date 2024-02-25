1a248e120a2b740c3d9f61fe101f93507e0ff3ae
Elena S <prostovael@yandex.ru>
2024-02-26 00:08:37 +0400

    src/BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class BonusMilesService {
+    public int calculate(int price) {
+
+        int bonus = 20; // количество рублей для одной бонусной мили;
+
+        return price / bonus;
+    }
 }
